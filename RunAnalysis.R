3574222ccdbf2d90734112d4c939c5654ca5d12b
Pinja-Liina Jalkanen <plj@iki.fi>
2020-03-04 19:55:07 +0200

    Added some plotting code. Needs work.

--- RunAnalysis.R
+++ RunAnalysis.R
@@ -7,6 +7,9 @@ rm(list = ls())
 library(RPostgres)
 #library(GGally)
 library(ggplot2)
+library(randomcoloR)
+library(directlabels)
+library(reshape2)
 
 # Reset graphical parameters and save the defaults.
 plot.new()
@@ -68,9 +71,59 @@ query_reg <- function(measure) {
 
 # Get data from the DB.
 agg_j_all_pt <- dbGetQuery(dbc, query_all("pt_m_tt"))
-agg_j_ic <- dbGetQuery(dbc, query_ic("pt_m_tt"))
-agg_j_reg <- dbGetQuery(dbc, query_reg("pt_m_tt"))
-
+agg_j_all_varnames <- c(
+  "Measure",
+  "TTM",
+  "JourneyYear",
+  "Total"
+)
+colnames(agg_j_all_pt) <- agg_j_all_varnames
+agg_j_ic_pt <- dbGetQuery(dbc, query_ic("pt_m_tt"))
+agg_j_ic_varnames <- c(
+  # Give the columns new names in English.
+  # The classification is based on the 2008 industry classification of Statistics Finland:
+  # https://www.stat.fi/meta/luokitukset/toimiala/001-2008/index_en.html
+  # Referenced 2020-03-04
+  "Measure",
+  "TTM",
+  "JourneyYear",
+  "Total",
+  "PriProd",   #Agriculture, forestry and fishing
+  "Mining",    #Mining and quarrying
+  "Manuf",     #Manufacturing
+  "ElAC",      #Electricity, gas, steam and air conditioning supply
+  "WaterEnv",  #Water supply; sewerage, waste management and remediation activities
+  "Construct", #Construction
+  "Trade",     #Wholesale and retail trade; repair of motor vehicles and motorcycles
+  "Logistics", #Transportation and storage
+  "HoReCa",    #Accommodation and food service activities
+  "InfoComm",  #Information and communication
+  "FinIns",    #Financial and insurance activities
+  "RealEst",   #Real estate activities
+  "SpecProf",  #Professional, scientific and technical activities (speciality professions, e.g. law, architecture, accounting, marketing, research, vets)
+  "AdminSupp", #Administrative and support service activities (e.g. leasing, office services, employment agencies, security, travel agencies)
+  "PubAdmDef", #Public administration and defence; compulsory social security
+  "Education", #Education
+  "HealthSoc", #Human health and social work activities
+  "ArtsEnt",   #Arts, entertainment and recreation
+  "OtherServ", #Other service activities (e.g. NGOs, appliance repair services, laundry services, spas) 
+  "HomeEmp",   #Activities of households as employers; undifferentiated goods- and services-producing activities of households for own use
+  "IntOrgs",   #Activities of extraterritorial organisations and bodies
+  "UnknownInd" #Unknown industry
+)
+colnames(agg_j_ic_pt) <- agg_j_ic_varnames
+agg_j_reg_pt <- dbGetQuery(dbc, query_reg("pt_m_tt"))
+agg_j_reg_varnames <- c(
+  "Measure",
+  "TTM",
+  "JourneyYear",
+  "MunID",
+  "AreaID",
+  "DistID",
+  "RegID",
+  "Total"
+)
+colnames(agg_j_reg_pt) <- (agg_j_reg_varnames)
 
 # Travel habits in the Helsinki Region 2018: relative share of public
 # transport journeys of all motorised journeys (Brandt et al. 2018: appendix 4, p. 10):
@@ -78,6 +131,41 @@ method_titles <- list(c("Other", "Walk", "Bike", "PT", "Car"), c("2012", "2018")
 method_share <- matrix(c(2,26.8,7.2,27.2,36.1,0.4,30.5,9.2,25.4,34.0), nrow = 5, dimnames = method_titles)
 method_share
 
-# Try to plot something…
-# ggplot(agg_j_all_pt, aes(x = factor(year), y=as.double(total), group=1)) + geom_point() +
-#   stat_summary(fun.y=sum, geom="line") + xlab("Year") + ylab("Aggregated journey time (min)")
+# Try to plot something… this plotting method will most likely be redundant.
+# agg_j_plot <- function(df, vars, legend) {
+#   plot <- ggplot(df, aes(x = factor(TTM), group=1))
+#   for(ind in variable.names(df)[vars]) {
+#     plot <- plot + geom_line(aes_string(y=ind, colour=p('"', ind, '"')), size=2)
+#   }
+#   plot <- plot + xlab("Year") + ylab("Aggregated journey time (min)")
+#   plot <- plot + labs(colour = legend)
+#   return(plot)
+# }
+
+agg_j_plot <- function(df, vars, legend = FALSE) {
+  # Plot values. If the legend is not false, show it for the vars. Else show directlabels.
+  df_long <- df
+  if(!(names(df[7]) == "RegID")) { #FIXME! Does not work, the num of cols is not known!
+     df_long <- melt(df[vars], id=names(df[2]))
+  }
+  else {
+    1 == 1
+  }
+  plot <- ggplot(df_long, aes(x=factor(TTM), group=variable, y=value, colour=variable)) +
+    geom_line(size=1.3) +
+    xlab("Year of the Time Travel Matrix") +
+    ylab("Aggregated time spend by all journeys")
+    if(legend == FALSE) {
+      plot <- plot +
+      geom_dl(aes(label = variable), method = list(dl.trans(x = x + 0.2), "last.qp", cex = 0.8)) +
+      geom_dl(aes(label = variable), method = list(dl.trans(x = x - 0.2), "first.qp", cex = 0.8)) +
+      theme(legend.position = "none")
+    }
+    else {
+      plot <- plot + labs(colour = legend)
+    }
+    return(plot)
+}
+
+agg_j_plot(agg_j_ic_pt, c(2,7,10:23))
+agg_j_plot(agg_j_all_pt, c(2,4), "Total")
