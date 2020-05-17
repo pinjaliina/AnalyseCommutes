99b69fc71e04bb56db1b8a0b9e1cb7ee6119c532
Pinja-Liina Jalkanen <plj@iki.fi>
2020-05-18 01:25:22 +0300

    Improved statistics output and map labels.

--- PlotMaps.py
+++ PlotMaps.py
@@ -9,6 +9,9 @@ Created on Wed Apr 15 15:26:30 2020
 @author: Pinja-Liina Jalkanen (pinjaliina@iki.fi)
 """
 
+# Silence FutureWarnings of Pandas
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 # Import necessary modules
 import pandas
 import geopandas as gpd   # geopandas is usually shortened as gpd
@@ -25,7 +28,8 @@ from bokeh.models import \
 import bokeh.palettes as palettes
 import psycopg2
 from os import path
-#import pandas.io.sql as psql
+import tempfile
+from scipy import stats
 
 def get_db_conn():
     """Define DB connection params.
@@ -61,10 +65,49 @@ def get_plot(query, conn, desc, first, last):
     # Classify data (manual classes based on outputs of previous runs!)
     breaks = [-10.1, 10, 101]
     classifier = mc.UserDefined.make(bins=breaks)
-    classes = df[['Change']].apply(classifier)
+    classes = df[['RFChange']].apply(classifier)
     classes.columns = ['Class']
     df = df.join(classes)
  
+    # Collect some statistics:
+    # For practical help how to do this in Python:
+    # https://pythonfordatascience.org/independent-t-test-python/
+    statistics = {
+        'min_rfchg': df['RFChange'].min(),
+        'max_rfchg': df['RFChange'].max(),
+        'min_abschg': df['AbsChange'].min(),
+        'max_abschg': df['AbsChange'].max(),
+        'mean_rfchg': df['RFChange'].mean(),
+        'mean_abschg': df['AbsChange'].mean(),
+        'median_abschg': df['AbsChange'].mean(),
+        'stdev_abschg': df['AbsChange'].std(),
+        'mean_T1': df['T1'].mean(),
+        'mean_T2': df['T2'].mean(),
+        'stdev_T1': df['T1'].std(),
+        'stdev_T2': df['T2'].std(),
+        'levenestat_abschg': None,
+        'levenepval_abschg': None,
+        'shapirostat_abschg': None,
+        'spapiropval_abschg': None,
+        't-stat_abschg': None,
+        't-pval_abschg': None,
+        't-df_abschg': None}
+    # None of this makes sense unless both variables have data:
+    if not ((df['T1'] == 0).all() or (df['T2'] == 0).all()):           
+        # Null hypothesis for Levene test: both inputs have equal variances.
+        statistics['levenestat_abschg'], \
+            statistics['levenepval_abschg'] = stats.levene(df['T1'], df['T2'])
+        # Null hypothesis for Shapiro-Wilk: normal distribution of residuals.
+        statistics['shapirostat_abschg'], \
+            statistics['shapiropval_abschg'] = stats.shapiro(df['T1']-df['T2'])
+        statistics['t-stat_abschg'], \
+            statistics['t-pval_abschg'] = stats.ttest_ind(df['T1'], df['T2'])
+        statistics['t-df_abschg'] = df['T1'].count() + df['T2'].count() - 2
+        # Do not use researchpy; it outputs a dataframe, and digging results
+        # out of that just adds complexity.
+        # statistics['descriptives_abs'], statistics['ttest_abs'] = rp.ttest(
+        #     df['T1'], df['T2'])
+
     # Define class names
     cnames = [
         '[-100…-10[',
@@ -133,11 +176,15 @@ def get_plot(query, conn, desc, first, last):
                            title='Change, %-p.'))
     
     hoverinfo = HoverTool()
-    hoverinfo.tooltips = [('Region', '@nimi'), ('Change, %-point', '@Change')]
+    hoverinfo.tooltips = [('Region', '@nimi'),
+                          ('Mean time ' + first,'@T1'),
+                          ('Mean time ' + last,'@T2'),
+                          ('Mean time change', '@AbsChange'),
+                          ('Change, %-point of the industry total', '@RFChange')]
+
     plot.add_tools(hoverinfo)
     
-    return plot, df['Change'].min(), df['Change'].max()
-
+    return plot, statistics
 
 def industry_list():
     return {
@@ -164,49 +211,87 @@ def industry_list():
         'IntOrgs': 'International organisations',
         'UnknownInd': 'Unknown industry'}
 
-# Get a DB connection object
+# Get a DB connection object.
 pg = get_db_conn()
 
 modes = {'car': 'Car', 'pt': 'PT'}
 
 series = ('2013', '2015', '2018')
 
+plot_stats_ind = list()
+
 for modeid, mode in modes.items():
     plots = list()
-    plot_stats = list()
+    
     
     for field, desc in industry_list().items():
-        query = 'SELECT * FROM (SELECT CASE WHEN r.nimi<>\'\' THEN r.nimi \
-    ELSE \'Kauniainen\' END AS nimi,r.geom,j1."MunID",j1."AreaID",j1."DistID",\
-    j1."Count" AS "C1",j1."' + field +'" AS "T1",\
-    j2."Count" AS "C2",j2."' + field + '" AS "T2",\
-    ROUND(CAST(j2."' + field + '"-j1."' + field + '" AS NUMERIC), 2\
-        ) AS "Change" \
-    FROM hcr_subregions r \
-    LEFT JOIN res_agg_j_ic_reg_' + modeid + '_icfreq_mun j1 ON \
-    r.kokotun=j1."RegID" AND j1."TTM" = {} \
-    LEFT JOIN res_agg_j_ic_reg_' + modeid + '_icfreq_mun j2 ON \
-    r.kokotun=j2."RegID" AND j2."TTM" = {}) AS Q WHERE "Change" IS NOT NULL'
+        query=(
+            'SELECT * FROM (SELECT '
+                'CASE WHEN '
+                'r.nimi<>\'\' THEN r.nimi ELSE \'Kauniainen\' END AS nimi, '
+                'r.geom, '
+                'rf1."MunID", '
+                'rf1."AreaID", '
+                'rf1."DistID", '
+                'rf1."Count" AS "C1", '
+                'rf1."' + field +'" AS "RF_T1", '
+                'abs1."' + field +'" AS "T1", '
+                'CASE WHEN abs1."' + field +'" <> 0 THEN '
+                'ROUND(CAST('
+                	'abs1."' + field +'"/(abs1."' + field +\
+                    '"/abs1."Total")/abs1."Count" AS NUMERIC), 2) '
+                'ELSE 0 END AS "M1", '
+                'rf2."Count" AS "C2",rf2."' + field + '" AS "RF_T2", '
+                'abs2."' + field +'" AS "T2", '
+                'CASE WHEN abs2."' + field +'" <> 0 THEN '
+                'ROUND(CAST('
+                	'abs2."' + field +'"/(abs2."' + field + \
+                    '"/abs2."Total")/abs2."Count" AS NUMERIC), 2) '
+                'ELSE 0 END AS "M2", '
+                'ROUND(CAST(rf2."' + field + '"-rf1."' + field + \
+                    '" AS NUMERIC), 2) AS "RFChange", '
+                'ROUND(CAST(abs2."' + field + '"-abs1."' + field + \
+                    '" AS NUMERIC), 2) AS "AbsChange" '
+                'FROM hcr_subregions r '
+                'LEFT JOIN res_agg_j_ic_reg_' + modeid + '_icfreq_mun rf1 ON '
+                    'r.kokotun=rf1."RegID" AND rf1."TTM" = {} '
+                'LEFT JOIN res_agg_j_ic_reg_' + modeid + '_icfreq_mun rf2 ON '
+                    'r.kokotun=rf2."RegID" AND rf2."TTM" = {} '
+                'LEFT JOIN res_agg_j_ic_reg_' + modeid + '_mun abs1 ON '
+                    'r.kokotun=abs1."RegID" AND abs1."TTM" = {} '
+                'LEFT JOIN res_agg_j_ic_reg_' + modeid + '_mun abs2 ON '
+                    'r.kokotun=abs2."RegID" AND abs2."TTM" = {} '
+            ') AS Q WHERE "AbsChange" IS NOT NULL')
     
         for i, item in enumerate(series):
             if(i<=len(series)-2):
-                q = query.format(item, series[i+1])
+                q = query.format(item, series[i+1], item, series[i+1])
                 plot = get_plot(
                     q, pg,
                     mode + ': Change of the share of the total time: ' + desc,
                     item, series[i+1])
                 plots.append(plot[0])
-                plot_stats.append([field, str(item) + '–' + str(series[i+1]),
-                                   plot[1], plot[2]])
-        
-    # Print stats
-    print('Descriptives for Mode: ' + mode)
-    psdf = pandas.DataFrame(plot_stats, columns=['Ind','Period','Min','Max'])
-    print(psdf.describe())
-    print(psdf[psdf['Period']=='2013–2015'].describe())
-    print(psdf[psdf['Period']=='2015–2018'].describe())
-                
-    # Save the map
+                statsdic = {'mode': mode,
+                            'indcode': field,
+                            'ind_desc': desc,
+                            'period': str(item) + '–' + str(series[i+1])}
+                statsdic.update(plot[1])
+                plot_stats_ind.append(statsdic)
+                         
+    # Save the map.
     outfp = path.join(path.dirname(path.realpath(__file__)),
                       'docs/icfreq_reg_maps_' + modeid + '.html')
     save(gridplot(plots, ncols=2), outfp, title='IC frequency plots, ' + mode)
+    
+# Write all statistics to an xlsx file.
+# The file will be located in a temporary directory, which on some platforms
+# is under a funny path. But the script will print the full path to the file.
+plot_stats_dict = dict()
+for k in plot_stats_ind[0].keys():
+    plot_stats_dict[k] = tuple(
+        plot_stats_dict[k] for plot_stats_dict in plot_stats_ind)
+plot_stats = pandas.DataFrame(plot_stats_dict)
+statsfp = path.join(tempfile.gettempdir(),
+                  'ic_maps_stats.xlsx')
+plot_stats.to_excel(statsfp)
+print('Statistics written to file "' + statsfp + '".')
